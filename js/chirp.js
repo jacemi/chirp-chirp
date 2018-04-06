a1f75c1a4d981bb2bcc089fa99ed585407dff275
Jace Mitsuka <PJace@BigMac.local>
2018-04-06 13:41:35 -1000

    complete exercise

--- js/chirp.js
+++ js/chirp.js
@@ -1,6 +1,10 @@
-function chirp(n){
-  // FIX ME
-}
+function chirp(n) {
+
+    if (n === 0) {
+      return ""
+    }
+    return "chirp " + chirp(--n)
+  }
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
